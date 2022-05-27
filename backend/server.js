2417e8ad95d1b90b58def50d5dd6e44bbe30c663
Michelle <michelleevarogers@gmail.com>
2022-05-27 12:48:01 -0700

    added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,34 @@
 const PORT = process.env.PORT || 5000;
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const userRoutes = require("./routes/userRoutes");
 
+// Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// Listen
+
 app.listen(PORT, () => {
-  console.log(`SmartBrain Backend is listening on port ${PORT}...`);
+  console.log(`> SmartBrain API is listening on port ${PORT}...`);
 });
+
+// Root Route
 
 app.get("/", (req, res) => {
-  res.send("This is working");
+  res.json({ message: "SmartBrain API" });
 });
 
-app.post("/signin", (req, res) => {
-  res.json();
-});
+// User Routes
 
 app.use("/api/users", userRoutes);
