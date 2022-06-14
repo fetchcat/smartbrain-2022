6da3f451e3a2c15c3a01193fefa8c6ced2ca9e8e
Michelle <michelleevarogers@gmail.com>
2022-06-14 12:23:22 -0700

    initial commit

---
+++ backend-psql/server.js
@@ -0,0 +1,35 @@
+const PORT = process.env.PORT || 5000;
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const userRoutes = require("./routes/userRoutes");
+
+// Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    // Domain Whitelist
+    origin: process.env.ORIGIN,
+  })
+);
+
+// Listen
+
+app.listen(PORT, () => {
+  console.log(`> SmartBrain API is listening on port ${PORT}...`);
+});
+
+// Root Route
+
+app.get("/", (req, res) => {
+  res.json({ message: "SmartBrain API" });
+});
+
+// User Routes
+
+app.use("/api/users", userRoutes);
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,51 @@
-const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+
+// --- ENV VAR --- //
+
+const port = process.env.PORT || 5000;
+
+// --- MySQL DB --- //
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DBUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+// --- Express --- //
 
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const router = express.Router();
 
-const userRoutes = require("./routes/userRoutes");
+app.listen(port, () => {
+  console.log(`> SmartBrain Backend listening on port: ${port}...`);
+});
 
-// Middleware
+// --- Middleware --- //
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    // Domain Whitelist
-    origin: process.env.ORIGIN,
-  })
-);
 
-// Listen
-
-app.listen(PORT, () => {
-  console.log(`> SmartBrain API is listening on port ${PORT}...`);
-});
-
-// Root Route
+// --- Default Route --- //
 
 app.get("/", (req, res) => {
-  res.json({ message: "SmartBrain API" });
+  res.status(200).send("SmartBrain Backend").end();
 });
 
-// User Routes
+// Test
 
-app.use("/api/users", userRoutes);
+app.get("/test", async (req, res) => {
+  const sqlQuery = "SELECT * FROM users";
+  const response = await connection.query(
+    sqlQuery,
+    function (err, results, fields) {
+      console.log(results);
+      console.log(fields);
+    }
+  );
+  res.status(200).json(response);
+});
