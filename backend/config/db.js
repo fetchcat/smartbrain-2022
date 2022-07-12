6f5c622273793969b01da77ace9817671ffc1803
Michelle Rogers <michelleevarogers@gmail.com>
2022-07-12 16:42:37 -0700

    added comment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const Pool = require("pg").Pool;
 
+// PostgreSQL Connection Pool
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
